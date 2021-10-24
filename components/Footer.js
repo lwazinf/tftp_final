26ba7a75a11764714fda5af49d2706d20bd8095f
Lwazi Ndlovu <42185877+lwazin@users.noreply.github.com>
2021-10-24 12:39:59 +0200

    Half finished Footer, again

--- components/Footer.js
+++ components/Footer.js
@@ -9,12 +9,12 @@ const Footer = () => {
                     <div className={styles.footerColHeading}>Support</div>
                     <div className={styles.footerColSub}>Contact Us</div>
                     <div className={styles.footerColSub}>Locate Us</div>
+                    <div className={styles.footerColSub}>Downloads</div>
                     <div className={styles.footerColSub}>FAQ</div>
                 </div>
                 <div className={styles.footerCol}>
                     <div className={styles.footerColHeading}>Services</div>
                     <div className={styles.footerColSub}>About Ten Flags</div>
-                    <div className={styles.footerColSub}>Downloads</div>
                     <div className={styles.footerColSub}>Functions</div>
                     <div className={styles.footerColSub}>Partners</div>
                     <div className={styles.footerColSub}>Careers</div>
