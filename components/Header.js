a060f5b8811cd6716e3fe19a9b69f82ff77d7905
Lwazi Ndlovu <42185877+lwazin@users.noreply.github.com>
2021-11-11 16:25:36 +0200

    Nav done

--- components/Header.js
+++ components/Header.js
@@ -9,20 +9,22 @@ const Header = () => {
     return (
         <div className={styles.container}>
             <div className={styles.logo}>
-                <div className={styles.logoImage}>
-                    <Image src={"/assets/tftp_logo.png"} alt={"logo"} height={120} width={120} />
-                </div>
+                <Image src={"/assets/tftp_logo.png"} alt={"logo"} height={123} width={123} />
             </div>
+            
             <div className={styles.hud}>
-                <Link activeClass="active" className="hero" to="hero" spy={true} smooth={true} offset={-150} duration={500}>
+                <Link activeClass="active" className="hero" to="hero" spy={true} smooth={true} offset={-170} duration={500}>
                     <p className={styles.hudItem}>Home</p>
                 </Link>
-                <Link activeClass="active" className="activities" to="activities" spy={true} smooth={true} offset={-200} duration={500}>
+                <p className={styles.hudDiv}>||</p>
+                <Link activeClass="active" className="activities" to="activities" spy={true} smooth={true} offset={-240} duration={500}>
                     <p className={styles.hudItem}>Activities</p>
                 </Link>
-                <Link activeClass="active" className="blog" to="blog" spy={true} smooth={true} offset={-150} duration={500}>
+                <p className={styles.hudDiv}>||</p>
+                <Link activeClass="active" className="blog" to="blog" spy={true} smooth={true} offset={-170} duration={500}>
                     <p className={styles.hudItem}>Blog</p>
                 </Link>
+                <p className={styles.hudDiv}>||</p>
                 <Link activeClass="active" className="about" to="about" spy={true} smooth={true} offset={-220} duration={500}>
                     <p className={styles.hudItem}>About</p>
                 </Link>
